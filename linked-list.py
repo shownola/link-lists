8ee112be075db434ed0da2a6718d232735c01894
shownola <swasieko@gmail.com>
2019-09-13 15:48:41 -0700

    build a custom linked list

--- linked-list.py
+++ linked-list.py
@@ -5,4 +5,72 @@ class Node:
         self.next = None
 
     def __str__(self):
-        return str()
+        return f"{self.data}"
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append_val(self, x):
+        """add x to the end of the list"""
+        if not isinstance(x, Node):
+            x = Node(x)
+        if self.head == None:
+            self.head = x
+        else:
+            self.tail.next = x
+        self.tail = x
+
+    def __str__(self):
+        # [5->4->10->1]
+        to_print = ""
+        curr = self.head
+        while curr is not None:
+            to_print += str(curr.data) + "->"
+            curr = curr.next
+        if to_print:
+            return "[" + to_print[:-2] + "]"
+        return "[]"
+
+    def add_to_start(self, x):
+        """add x to the left of the list making it the head"""
+        pass
+
+    def search_val(self, x):
+        """return indices where x was found"""
+        pass
+
+    def remove_val_by_index(self, x):
+        """remove and return value at index x provided as parameter"""
+        pass
+
+    def length(self):
+        """return the length of the list, rep'd by number of nodes"""
+        pass
+
+    def reverse_list_recur(self, current, previous):
+        """reverse the sequence of node pointers in the linked list"""
+        pass
+
+
+
+
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+my_list = LinkedList()
+
+print(my_list)
+my_list.append_val(node1)
+my_list.append_val(node2)
+my_list.append_val(node3)
+my_list.append_val(node4)
+my_list.append_val(5)
+print(my_list)
